cfe43fea84c12162633a9a359d72c568405e3dbd
Ldalvik <subaruforge@gmail.com>
2022-08-22 17:49:23 -0400

    Created add deck form

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,4 +1,24 @@
 class DecksController < ApplicationController
-      
+    def new
+        @deck = Deck.new
+        render :"fastcards/new"
+    end
+
+    def create
+      @deck = Deck.create(deck_params)
+      if @deck.save
+          flash[:msg] = "Deck added successfully"
+          # redirect_to "/deck/#{@deck.id}"
+          redirect_to "/decks"
+      else
+          flash.now[:msg] = @deck.errors.full_messages.to_sentence
+          render :"fastcards/new"
+      end
+    end
+
+
+    def deck_params      
+        params.require(:deck).permit(:name, :category, :description, :difficulty)
+    end
 end
   
